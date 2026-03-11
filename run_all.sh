266210425ace9656b8758bdbdb4dbd636b5f309d
huxiaoou <516984451@qq.com>
2026-03-11 09:32:58 +0800

    Rename destination tables

--- run_all.sh
+++ run_all.sh
@@ -16,7 +16,7 @@ bgn_date="20180102"
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] try removing existing data"
 
 cls="c2"
-rm_tqdb huxiaoou_private --table sector_${cls}_d
+rm_tqdb huxiaoou_private --table sector_index_${cls}_d
 python main.py $cls --bgn $bgn_date --end $end_date --freq d
-rm_tqdb huxiaoou_private --table sector_${cls}_m
+rm_tqdb huxiaoou_private --table sector_index_${cls}_m
 python main.py $cls --bgn $bgn_date --end $end_date --freq m
--- typedef.py
+++ typedef.py
@@ -54,7 +54,7 @@ class CSectorClassification:
     def get_save_data_desc(self, db_name: str, freq: TFreq) -> CDataDescriptor:
         return CDataDescriptor(
             db_name=db_name,
-            table_name=f"sector_{self.comb_name(freq)}",
+            table_name=f"sector_index_{self.comb_name(freq)}",
             codes=self.sectors,
             fields=["ret", "close"],
             lag=20,
