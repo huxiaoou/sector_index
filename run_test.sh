3d5c6e5e4b08c1d7543678dc5ddaaa159d83540d
huxiaoou <516984451@qq.com>
2026-03-11 09:33:45 +0800

    Update end_date in run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -2,5 +2,5 @@
 
 rm_tqdb huxiaoou_private --table sector_index_c0_d
 echo $(date +"%Y-%m-%d %H:%M:%S")
-python main.py c0 --bgn 20180102 --end 20260122 --freq d
+python main.py c0 --bgn 20180102 --end 20260310 --freq d
 echo $(date +"%Y-%m-%d %H:%M:%S")
